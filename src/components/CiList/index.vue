e05d528aa9b41c3aec671e52b2f1342cd47749c8
jeasonzone <943745703@qq.com>
2019-10-11 13:43:09 +0800

    接口数据

--- src/components/CiList/index.vue
+++ src/components/CiList/index.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="cinema_body">
         <ul>
-            <li>
+            <li v-for="item in cinemaList" v-bind:key="item.id">
                 <div>
-                    <span>大地影院(澳东世纪店)</span>
-                    <span class="q"><span class="price">22.9</span> 元起</span>
+                    <span>{{item.nm}}</span>
+                    <span class="q"><span class="price">{{item.sellPrice}}</span>元起</span>
                 </div>
                 <div class="address">
-                    <span>金州区大连经济技术开发区澳东世纪3层</span>
-                    <span>1763.5km</span>
+                    <span>{{item.addr}}</span>
+                    <span>{{item.distance}}</span>
                 </div>
                 <div class="card">
-                    <div>小吃</div>
-                    <div>折扣卡</div>
+                    <div v-if="item.tag.snack===1">小吃</div>
+                    <div v-if="item.tag.vipTag">折扣卡</div>
                 </div>
             </li>
         </ul>
@@ -21,7 +21,20 @@
 
 <script>
 export default {
-    name : 'CiList'
+    name : 'CiList',
+    data(){
+        return{
+            cinemaList:[]
+        }
+    },
+    mounted(){
+        this.axios.get('/api/cinemaList?cityId=10').then((res)=>{
+            var msg=res.data.msg;
+            if(msg==='ok'){
+                this.cinemaList=res.data.data.cinemas
+            }
+        })
+    }
 };
 </script>
 <style scoped>
